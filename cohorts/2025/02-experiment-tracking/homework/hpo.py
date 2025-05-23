6b7292a0b515a40bbca9b29fab49e5818210427f
Mykhailo Levytskyi <qwertymaden@gmail.com>
2025-05-23 11:25:47 +0200

    Add loging to HPO

--- cohorts/2025/02-experiment-tracking/homework/hpo.py
+++ cohorts/2025/02-experiment-tracking/homework/hpo.py
@@ -34,13 +34,18 @@ def run_optimization(data_path: str, num_trials: int):
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
     def objective(params):
+        with mlflow.start_run():
+            print(f"MLflow run started with params: {params}")
+            rf = RandomForestRegressor(**params)
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_val)
+            mse = mean_squared_error(y_val, y_pred)
+            rmse = mse ** 0.5
 
-        rf = RandomForestRegressor(**params)
-        rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_val)
-        rmse = mean_squared_error(y_val, y_pred, squared=False)
+            mlflow.log_params(params)
+            mlflow.log_metric("rmse", rmse)
 
-        return {'loss': rmse, 'status': STATUS_OK}
+            return {'loss': rmse, 'status': STATUS_OK}
 
     search_space = {
         'max_depth': scope.int(hp.quniform('max_depth', 1, 20, 1)),
